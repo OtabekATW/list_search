3c4409ddf122618926568ca22980b60ed4d9fef6
OtabekATW <otabektursunov468@gmail.com>
2023-11-29 11:55:15 +0500

    find03_max_min_sum.py

--- find03_max_min_sum.py
+++ find03_max_min_sum.py
@@ -5,4 +5,16 @@ def find_max_min_sum(data):
         data: list of numbers
     returns: sum of the maximum and minimum numbers in the list
     """
-    return 0
+    i = 0
+    mx = data[i]
+    mn = data[i]
+    while i < len(data):
+
+        if mx < data[i]:
+            mx = data[i]
+
+        if mn > data[i]:
+            mn = data[i]
+        i += 1
+
+    return mx + mn
