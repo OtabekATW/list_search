c5050bb4de2bbbaf141d46b21af6fbfd2913f075
OtabekATW <otabektursunov468@gmail.com>
2023-11-29 16:26:49 +0500

    find06_max_odd.py

--- find06_max_odd.py
+++ find06_max_odd.py
@@ -6,12 +6,20 @@ def find_max_odd(data):
     returns: maximum odd number in the list
     """
     i = 0
-    mx_odd = data[i]
+    list1 = []
 
     while i < len(data):
 
-        if data[i] % 2 == 1 and mx_odd < data[i]:
-            mx_odd = data[i]
-
+        if data[i] % 2 == 1:
+            list1.append(data[i])
         i += 1
+    
+    k = 0
+    mx_odd = list1[k]
+    while i < len(list1):
+
+        if mx_odd < list1[k]:
+            mx_odd = list1[k]
+        k += 1
+
     return mx_odd
