f340f13a7a13b2d673ab84ac7191e15d97aa2291
OtabekATW <otabektursunov468@gmail.com>
2023-11-29 13:29:21 +0500

    find04_max_index.py

--- find04_max_index.py
+++ find04_max_index.py
@@ -5,4 +5,11 @@ def find_max_index(data):
         data: list of numbers
     returns: index of maximum number in the list
     """
-    return 0
+    i = 0
+    mx = data[i]
+    while i < len(data):
+
+        if mx < data[i]:
+            mx = data[i]
+        i += 1
+    return data.index(mx)
