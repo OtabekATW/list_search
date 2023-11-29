f5e9b42015b5278f4889134b4a217dccca65e904
OtabekATW <otabektursunov468@gmail.com>
2023-11-29 16:04:08 +0500

    find05_max_even.py

--- find05_max_even.py
+++ find05_max_even.py
@@ -7,12 +7,21 @@ def find_max_even(data):
     """
     i = 0
     mx_even = data[i]
+    list1 = []
 
     while i < len(data):
 
-        if data[i] % 2 == 0 and mx_even < data[i]:
-            mx_even = data[i]
-
+        if data[i] % 2 == 0:
+            list1.append(data[i])
         i += 1
+
+    k = 0
+    mx_even = list1[k]
+    while k < len(list1):
+
+        if mx_even < list1[k]:
+            mx_even = list1[k]
+
+        k += 1
 
     return mx_even
