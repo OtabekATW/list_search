30755a4851db032fdfcc08a932545568b456de76
OtabekATW <otabektursunov468@gmail.com>
2023-11-29 15:53:51 +0500

    find10_min_even.py

--- find10_min_even.py
+++ find10_min_even.py
@@ -5,5 +5,21 @@ def find_min_even(data):
         data: list of numbers
     returns: minimum even number in the list
     """
-    return 0
+    i = 0
+    list1 = []
+    while i < len(data):
 
+        if data[i] % 2 == 0:
+            list1.append(data[i])
+        i += 1
+
+    k = 0
+    mn_even = list1[k]
+    while k < len(list1):
+
+        if list1[k] < mn_even:
+            mn_even = list1[k]
+        
+        k += 1
+
+    return mn_even
